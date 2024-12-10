143e3b22ef26595498aad639fcdf6ef5ac703164
MarkiitosTuFavorito <maraguale3@alu.edu.gva.es>
2024-12-10 09:52:50 +0100

    IES10D

---
+++ phyton3_5.py
@@ -0,0 +1,2 @@
+n1 = int(input("Dame un número "))
+print ("El resultado de elevar a 2 el número que me has dado es: ", 2**n1 )
---
+++ phyton3_7.py
@@ -0,0 +1,3 @@
+n1 = int(input("Dame un número real "))
+n2 = int(input("Dame otro número real "))
+print ("Los decimales de el resultado de dividir estos dos números es: ", (n1/n2)-(n1//n2))
