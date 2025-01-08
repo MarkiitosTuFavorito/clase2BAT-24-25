018fbdd400eab189d4d04c5b2db8af166104ed82
MarkiitosTuFavorito <maraguale3@alu.edu.gva.es>
2025-01-08 13:43:00 +0100

    IES8E

---
+++ phyton5/phyton5_1.py
@@ -0,0 +1,8 @@
+n = int(input("Dame un número entero: "))
+
+if n > 0 :
+    print("Este número es positivo")
+elif  n < 0:
+    print("Este número es negativo")
+else :
+    print("Este número es 0")
